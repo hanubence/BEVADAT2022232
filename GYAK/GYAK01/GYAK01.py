12e1d53a82606f5be3f872969597d03a670e3371
hanubence <hanuszkabence@gmail.com>
2023-03-03 13:33:39 +0100

    class work done

---
+++ GYAK/GYAK01/GYAK01.py
@@ -0,0 +1,48 @@
+# %%
+#Create a function that decides if a list contains any odd numbers.
+#return type: bool
+#function name must be: contains_odd
+#input parameters: input_list
+
+# %%
+def contains_odd(input_list):
+    return any(n % 2 != 0 for n in input_list)
+
+# %%
+#Create a function that accepts a list of integers, and returns a list of bool.
+#The return list should be a "mask" and indicate whether the list element is odd or not.
+#(return should look like this: [True,False,False,.....])
+#return type: list
+#function name must be: is_odd
+#input parameters: input_list
+
+# %%
+def is_odd(input_list):
+    return [True if n % 2 != 0 else False for n in input_list]
+
+# %%
+#Create a function that accpects 2 lists of integers and returns their element wise sum. <br>
+#(return should be a list)
+#return type: list
+#function name must be: element_wise_sum
+#input parameters: input_list_1, input_list_2
+
+# %%
+def element_wise_sum(input_list_1, input_list_2):
+    return(sum(input_list_1 + input_list_2))
+
+# %%
+#Create a function that accepts a dictionary and returns its items as a list of tuples
+#(return should look like this: [(key,value),(key,value),....])
+#return type: list
+#function name must be: dict_to_list
+#input parameters: input_dict
+
+# %%
+def dict_to_list(input_dict):
+    return [(k,v) for (k,v) in input_dict.items()]
+
+# %%
+#If all the functions are created convert this notebook into a .py file and push to your repo
+
+
