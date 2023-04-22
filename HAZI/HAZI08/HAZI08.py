13a35b573c4889abdfb27b3c238c378617944eb2
hanubence <32911312+hanubence@users.noreply.github.com>
2023-04-22 09:13:24 +0200

    Update HAZI08.py

--- HAZI/HAZI08/HAZI08.py
+++ HAZI/HAZI08/HAZI08.py
@@ -2,7 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import sklearn as sklearn
 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
