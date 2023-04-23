4bff828409bc6a4c069f57f8aa1b1cbd35175bdb
hanubence <hanuszkabence@gmail.com>
2023-04-23 18:03:21 +0200

    [FIX] Fixed GYAK08

--- GYAK/GYAK08/GYAK08.py
+++ GYAK/GYAK08/GYAK08.py
@@ -1,6 +1,6 @@
 from LinearRegressionSkeleton import LinearRegression
 
-model = LinearRegression(1000, 0.0001)
+model = LinearRegression(3000, 0.0005)
 
 model.fit(model.X_train, model.y_train)
 print(model.predict(model.X_test))
