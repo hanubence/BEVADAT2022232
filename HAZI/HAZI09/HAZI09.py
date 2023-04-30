e73def5f32828f1ed6da7e71c5eea899edccb80d
hanubence <hanuszkabence@gmail.com>
2023-04-30 21:43:35 +0200

    [FIX] Fixes to HAZI09

--- HAZI/HAZI09/HAZI09.py
+++ HAZI/HAZI09/HAZI09.py
@@ -4,6 +4,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.datasets import load_digits
+from scipy.stats import mode
 
 class KMeansOnDigits():
 
@@ -15,15 +16,13 @@ class KMeansOnDigits():
         self.digits = load_digits()
 
     def predict(self):
-        self.clusters = KMeans(n_clusters=self.n_clusters, n_init=10).fit_predict(self.digits.data, self.digits.target)
+        self.clusters = KMeans(n_clusters=self.n_clusters, n_init=10, random_state=self.random_state).fit_predict(self.digits.data, self.digits.target)
 
     def get_labels(self):
         self.labels = np.zeros_like(self.clusters)
-        for i in range(10):
-            mask = self.clusters == i
-            sub = self.digits.target[mask]
-            mode = np.bincount(sub).argmax()
-            self.labels[mask] = mode
+        for i in range(self.n_clusters):
+            mask = (self.clusters == i)
+            self.labels[mask] = mode(self.digits.target[mask], keepdims=True)[0]
 
     def calc_accuracy(self):
         self.accuracy = np.round(accuracy_score(self.labels, self.digits.target),2)
